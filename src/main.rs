8b7b80d31db8126334d45c475e37aa4cd46d9ee5
William Batista <bootlegbilly@protonmail.ch>
2021-08-12 17:05:17 +0000

    Added movement rules for Rook

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::convert::TryInto;
+use std::ops::Range;
 use macroquad::prelude::*;
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum PieceType {
     Pawn,
     Rook,
@@ -13,25 +13,12 @@ enum PieceType {
     Dead,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct Piece {
     piece_type: PieceType,
     position: (u8, u8),
     black: bool,
 
-
-}
-
-impl Piece {
-    fn new(piece_type: PieceType, position: (u8, u8), black: bool) -> Self {
-        Piece {
-           piece_type,
-           position,
-           black,
-            
-        }
-
-    }
 
 }
 
@@ -64,51 +51,21 @@ impl Evenness for u8 {
     }
 }
 
+// Some code I generated that contains the starting positions of all the pieces
+const STARTING_PIECES: [Piece; 32] = [Piece { piece_type: PieceType::Pawn, position: (0, 1), black: true }, Piece { piece_type: PieceType::Pawn, position: (1, 1), black: true }, Piece { piece_type: PieceType::Pawn, position: (2, 1), black: true }, Piece { piece_type: PieceType::Pawn, position: (3, 1), black: true }, Piece { piece_type: PieceType::Pawn, position: (4, 1), black: true }, Piece { piece_type: PieceType::Pawn, position: (5, 1), black: true }, Piece { piece_type: PieceType::Pawn, position: (6, 1), black: true }, Piece { piece_type: PieceType::Pawn, position: (7, 1), black: true }, Piece { piece_type: PieceType::Pawn, position: (0, 6), black: false }, Piece { piece_type: PieceType::Pawn, position: (1, 6), black: false }, Piece { piece_type: PieceType::Pawn, position: (2, 6), black: false }, Piece { piece_type: PieceType::Pawn, position: (3, 6), black: false }, Piece { piece_type: PieceType::Pawn, position: (4, 6), black: false }, Piece { piece_type: PieceType::Pawn, position: (5, 6), black: false }, Piece { piece_type: PieceType::Pawn, position: (6, 6), black: false }, Piece { piece_type: PieceType::Pawn, position: (7, 6), black: false }, Piece { piece_type: PieceType::Rook, position: (0, 7), black: false }, Piece { piece_type: PieceType::Knight, position: (1, 7), black: false }, Piece { piece_type: PieceType::Bishop, position: (2, 7), black: false }, Piece { piece_type: PieceType::Queen, position: (4, 7), black: false }, Piece { piece_type: PieceType::King, position: (3, 7), black: false }, Piece { piece_type: PieceType::Bishop, position: (5, 7), black: false }, Piece { piece_type: PieceType::Knight, position: (6, 7), black: false }, Piece { piece_type: PieceType::Rook, position: (7, 7), black: false }, Piece { piece_type: PieceType::Rook, position: (0, 0), black: true }, Piece { piece_type: PieceType::Knight, position: (1, 0), black: true }, Piece { piece_type: PieceType::Bishop, position: (2, 0), black: true }, Piece { piece_type: PieceType::King, position: (3, 0), black: true }, Piece { piece_type: PieceType::Queen, position: (4, 0), black: true }, Piece { piece_type: PieceType::Bishop, position: (5, 0), black: true }, Piece { piece_type: PieceType::Knight, position: (6, 0), black: true }, Piece { piece_type: PieceType::Rook, position: (7, 0), black: true }];
+
 #[macroquad::main("EmailChess")]
 async fn main() {
-    let starting_pieces: [Piece; 32] = {
-        let mut pieces: [Piece; 32] = [Piece::new(PieceType::Pawn, (0, 0), true); 32];
-
-        let specials = [PieceType::Rook, PieceType::Knight, PieceType::Bishop, PieceType::King, PieceType::Queen, PieceType::Bishop,PieceType::Knight, PieceType::Rook];
-
-        // Black pawns
-        for (i, piece) in pieces[0..8].iter_mut().enumerate() {
-            piece.position = (i.try_into().unwrap(), 1);
-    
-        }
-    
-        // White pawns
-        for (i, piece) in pieces[8..16].iter_mut().enumerate() {
-            piece.position = (i.try_into().unwrap(), 6);
-            piece.black = false;
-    
-        }
-
-        // White special pieces
-        for (i, (piece, special_piece)) in pieces[16..24].iter_mut().zip(specials.iter()).enumerate() {
-            piece.position = (i.try_into().unwrap(), 7);
-            piece.black = false;
-            piece.piece_type = *special_piece;
-
-        }
-
-        // Black special pieces
-        for (i, (piece, special_piece)) in pieces[24..32].iter_mut().zip(specials.iter()).enumerate() {
-            piece.position = (i.try_into().unwrap(), 0);
-            piece.piece_type = *special_piece;
-
-        }
-    
-        pieces
-    };
-
-    let mut pieces = starting_pieces;
+    let mut pieces = STARTING_PIECES;
     let piece_size = screen_height() / 10.0;
     let mut selected_piece: Option<(u8, u8)> = None;
 
+    #[cfg(debug_assertions)]
+    let mut debug_text_to_draw = String::new();
+
     loop {
         clear_background(DARKGRAY);
-        draw_text("CHESS", screen_width() / 2.0, 200.0, 50.0, BLACK);
+        draw_text(&debug_text_to_draw, screen_width() / 2.0, 200.0, 10.0, BLACK);
 
         let mouse_down = is_mouse_button_pressed(MouseButton::Left);
 
@@ -132,25 +89,24 @@ async fn main() {
                 // Don't let pieces move on top of other pieces, however
 
                 if selected_piece.is_some() {
-                    let (can_move, should_kill) = {
+                    let (can_move, can_kill) = {
                         let piece = pieces.iter().find(|piece| piece.position == selected_piece.unwrap() && piece.piece_type != PieceType::Dead).unwrap();
                         let piece_under_mouse = pieces.iter().find(|piece| piece.position == hovered_piece_pos && hovered_piece.1.unwrap() != PieceType::Dead);
 
-                        let mut moved_diagnally = false;
-
                         // First, check if the move is a move that this piece can usually make
-                        let legal_move = match piece.piece_type {
+                        let (legal_move, can_kill) = match piece.piece_type {
                             PieceType::Pawn => {
+                                let mut can_kill = false;
+
                                 // Basically, pawns can only move forward, or diagnally if you're killing an enemy piece
                                 // Make sure the pawn is only moving 1 space vertically, no matter what kind of move they're making
-                                (
-                                    match piece.black {
-                                        // White pawns can only move up
-                                        true => hovered_piece_pos.1 > piece.position.1 && hovered_piece_pos.1 - piece.position.1 == 1,
-                                        // Black pawns can only move down
-                                        false => hovered_piece_pos.1 < piece.position.1 && piece.position.1 - hovered_piece_pos.1 == 1,
-                                    }
-                                ) &&
+                                let legal_move = match piece.black {
+                                    // White pawns can only move up
+                                    true => hovered_piece_pos.1 > piece.position.1 && hovered_piece_pos.1 - piece.position.1 == 1,
+                                    // Black pawns can only move down
+                                    false => hovered_piece_pos.1 < piece.position.1 && piece.position.1 - hovered_piece_pos.1 == 1,
+                                }
+                                &&
 
                                 (
                                     // Check that the pawn is moving straight forward
@@ -159,29 +115,98 @@ async fn main() {
                                     // If the player isn't moving straight forward, then do some checks
                                     || 
                                     (
-                                        {
-                                            moved_diagnally = 
+                                        { 
+                                            can_kill = true;
+
                                             // First, check that there is an enemy piece where the player is trying to move
                                             piece_under_mouse.is_some() && 
                                             // Make sure that if the player is trying to move diagnally, they're only trying to move by 1 left or right
-                                            (piece_under_mouse.unwrap().position.0 + 1 == piece.position.0 || piece_under_mouse.unwrap().position.0 - 1 == piece.position.0);
-
-
-                                            moved_diagnally
+                                            (piece_under_mouse.unwrap().position.1 + 1 == piece.position.1 || piece_under_mouse.unwrap().position.1 - 1 == piece.position.1)
+                                            
                                         }
                                     )
-                                )
+                                );
+
+                                (legal_move, can_kill)
 
                             },
-                            _ => false,
+                            PieceType::Rook => {
+                                let no_piece_between_vert = |range_to_block_rook: Range<u8>| {
+                                    pieces.iter().find(|other_piece|  {
+                                        // Obviously, for a piece to block the rook, it needs to be on the same X axis (when moving vertically)
+                                        other_piece.position.0 == piece.position.0 && 
+                                        // Then, there can be no pieces in between where the player is trying to move and where it's moving
+                                        range_to_block_rook.contains(&other_piece.position.1) && 
+                                        // If the other piece is at the very exact position the rook was trying to move, it will kill it
+                                        other_piece.position.1 != hovered_piece_pos.1 &&
+                                        // Obviously, the piece can't be dead
+                                        other_piece.piece_type != PieceType::Dead
+                                    }).is_none()
+                                };
+
+                                let mut no_piece_between_horiz = |range_to_block_rook: Range<u8>| {
+                                    pieces.iter().find(|other_piece|  {
+                                        debug_text_to_draw = format!("{:?}", other_piece);
+                                        // Obviously, for a piece to block the rook, it needs to be on the same Y axis (when moving vertically)
+                                        other_piece.position.1 == piece.position.1 && 
+                                        // Then, there can be no pieces in between where the player is trying to move and where it's moving
+                                        range_to_block_rook.contains(&other_piece.position.0) && 
+                                        // If the other piece is at the very exact position the rook was trying to move, it will kill it
+                                        other_piece.position.0 != hovered_piece_pos.0 &&
+                                        // Obviously, the piece can't be dead
+                                        other_piece.piece_type != PieceType::Dead
+                                    }).is_none()
+                                };
+
+
+                                ((piece.position.0 == hovered_piece_pos.0 &&
+                                    // Check to see if the rook is moving up or down
+                                     match hovered_piece_pos.1 < piece.position.1 {
+                                         // Moving up
+                                        true => {
+                                            let range_to_block_rook = hovered_piece_pos.1..piece.position.1;
+                                            // There can't be any pieces between the place where the rook is, and where it's attempting to move
+                                            no_piece_between_vert(range_to_block_rook)
+
+                                        },
+                                        // Moving down
+                                        false => {
+                                            let range_to_block_rook = piece.position.1 + 1..hovered_piece_pos.1;
+                                            no_piece_between_vert(range_to_block_rook)
+                                            
+                                        },
+
+                                }) 
+                                // Rooks can move vertically or horizontally, but not both, hence the XOR
+                                ^
+                                 (piece.position.1 == hovered_piece_pos.1 && match hovered_piece_pos.0 < piece.position.0 {
+                                    // Left
+                                    true => {
+                                        let range_to_block_rook = hovered_piece_pos.0..piece.position.0;
+                                        // There can't be any pieces between the place where the rook is, and where it's attempting to move
+                                        no_piece_between_horiz(range_to_block_rook)
+
+                                    },
+                                    // Right
+                                    false => {
+                                        let range_to_block_rook = piece.position.0 + 1..hovered_piece_pos.0;
+                                        no_piece_between_horiz(range_to_block_rook)
+                                        
+                                    },
+
+                                 }), true)
+                            }
+                            _ => (false, false),
 
                         };
 
 
                         match legal_move {
+                            // Obviously, if it isn't a legal move, then don't let the player move at all
                             true => match piece_under_mouse {
-                                // One of the pieces can be black, but not both for it to be able to move
-                                Some(piece_under_mouse) => (piece.black ^ piece_under_mouse.black && moved_diagnally, moved_diagnally),
+                                // If it is a legal move, only move onto a piece if it's an enemy piece (piece.black ^ piece_under_mouse.black)
+                                // Also only move onto that piece if it's able to kill it
+                                Some(piece_under_mouse) => (piece.black ^ piece_under_mouse.black && can_kill, can_kill),
                                 None => (true, false)
                             }
                             false => (false, false)
@@ -192,12 +217,11 @@ async fn main() {
                     };
 
                     if can_move {
-                        if should_kill {
+                        if can_kill {
                             if let Some(piece_under_mouse) = pieces.iter_mut().find(|piece| piece.position == hovered_piece_pos) {
                                 piece_under_mouse.piece_type = PieceType::Dead;
 
                             }
-                            
 
                         }
 
@@ -205,9 +229,11 @@ async fn main() {
                         let piece = pieces.iter_mut().find(|piece| piece.position == selected_piece.unwrap() && piece.piece_type != PieceType::Dead).unwrap();
 
                         piece.position = hovered_piece_pos;
-                        selected_piece = None;
 
                     }
+
+                    // Whether the player moves or not, if they click after having a selected piece, then the selected piece shuold be reset 
+                    selected_piece = None;
 
                 // There is no piece selected, so it selects the piece the player is currently hovering over
                 } else if let Some(piece) = pieces.iter_mut().find(|piece| piece.position == hovered_piece_pos && piece.piece_type != PieceType::Dead) {
