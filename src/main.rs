904f64fed04a6cb3575644ae8040d5b64d8eed80
William Batista <bootlegbilly@protonmail.ch>
2021-08-12 08:38:56 -0400

    Added more pawn rules

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,17 @@ enum PieceType {
 struct Piece {
     piece_type: PieceType,
     position: (u8, u8),
-    color: Color,
+    black: bool,
+
 
 }
 
 impl Piece {
-    fn new(piece_type: PieceType, position: (u8, u8), color: Color) -> Self {
+    fn new(piece_type: PieceType, position: (u8, u8), black: bool) -> Self {
         Piece {
            piece_type,
            position,
-           color,
+           black,
             
         }
 
@@ -34,6 +35,7 @@ impl Piece {
 
 }
 
+#[inline]
 fn mouse_in_rectangle(coords: (f32, f32), size: (f32, f32)) -> bool {
     let mouse_pos = mouse_position();
     
@@ -45,28 +47,28 @@ fn mouse_in_rectangle(coords: (f32, f32), size: (f32, f32)) -> bool {
 }
 
 trait Evenness {
-    fn is_even(self) -> bool;
-    fn is_odd(self) -> bool;
+    fn is_even(&self) -> bool;
+    fn is_odd(&self) -> bool;
 
 }
 
 impl Evenness for u8 {
     #[inline(always)]
-    fn is_even(self) -> bool {
-        self & 1 == 0
+    fn is_even(&self) -> bool {
+        *self & 1 == 0
     }
 
     #[inline(always)]
-    fn is_odd(self) -> bool {
+    fn is_odd(&self) -> bool {
         !self.is_even()
     }
 }
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main("EmailChess")]
 async fn main() {
     let starting_pieces: [Piece; 32] = {
-        let mut pieces: [Piece; 32] = [Piece::new(PieceType::Pawn, (0, 0), BLACK); 32];
-        //let specials = [Piece::new(PieceType::Rook, (0, 0), BLACK), Piece::new(PieceType::Knight, (0, 0), BLACK), Piece::new(PieceType::Bishop, (0, 0), BLACK)];
+        let mut pieces: [Piece; 32] = [Piece::new(PieceType::Pawn, (0, 0), true); 32];
+
         let specials = [PieceType::Rook, PieceType::Knight, PieceType::Bishop, PieceType::King, PieceType::Queen, PieceType::Bishop,PieceType::Knight, PieceType::Rook];
 
         // Black pawns
@@ -78,22 +80,23 @@ async fn main() {
         // White pawns
         for (i, piece) in pieces[8..16].iter_mut().enumerate() {
             piece.position = (i.try_into().unwrap(), 6);
-            piece.color = WHITE;
+            piece.black = false;
     
         }
 
         // White special pieces
         for (i, (piece, special_piece)) in pieces[16..24].iter_mut().zip(specials.iter()).enumerate() {
             piece.position = (i.try_into().unwrap(), 7);
-            piece.color = WHITE;
+            piece.black = false;
             piece.piece_type = *special_piece;
+
         }
 
         // Black special pieces
         for (i, (piece, special_piece)) in pieces[24..32].iter_mut().zip(specials.iter()).enumerate() {
             piece.position = (i.try_into().unwrap(), 0);
-            piece.color = BLACK;
             piece.piece_type = *special_piece;
+
         }
     
         pieces
@@ -118,7 +121,6 @@ async fn main() {
                 draw_text(&format!("{},{}", hovered_piece_pos.0, hovered_piece_pos.1), screen_width() / 2.0, 300.0, 50.0, BLACK);
                 if let Some(p) = selected_piece {
                     draw_text(&format!("{},{}", p.0, p.1), screen_width() / 2.0, 250.0, 50.0, BLACK);
-                    draw_text(&format!("{}", pieces.iter().filter(|piece| piece.position == hovered_piece_pos).count()), screen_width() / 2.0, 150.0, 50.0, BLACK);
 
                 }
 
@@ -128,18 +130,91 @@ async fn main() {
             if mouse_down {
                 // Since a piece is already selected, we need to move the current piece
                 // Don't let pieces move on top of other pieces, however
-                if selected_piece.is_some() && pieces.iter().filter(|piece| piece.position == hovered_piece_pos).count() == 0 {
-                    let piece = pieces.iter_mut().find(|piece| piece.position == selected_piece.unwrap()).unwrap();
 
-                    piece.position = hovered_piece_pos;
-                    selected_piece = None;
+                if selected_piece.is_some() {
+                    let (can_move, should_kill) = {
+                        let piece = pieces.iter().find(|piece| piece.position == selected_piece.unwrap() && piece.piece_type != PieceType::Dead).unwrap();
+                        let piece_under_mouse = pieces.iter().find(|piece| piece.position == hovered_piece_pos && hovered_piece.1.unwrap() != PieceType::Dead);
 
-                // There is no piece selected, so it selects the one the player is currently hovering over
-                } else {
-                    if let Some(piece) = pieces.iter_mut().find(|piece| piece.position == hovered_piece_pos) {
-                        selected_piece = Some(piece.position);
+                        let mut moved_diagnally = false;
+
+                        // First, check if the move is a move that this piece can usually make
+                        let legal_move = match piece.piece_type {
+                            PieceType::Pawn => {
+                                // Basically, pawns can only move forward, or diagnally if you're killing an enemy piece
+                                // Make sure the pawn is only moving 1 space vertically, no matter what kind of move they're making
+                                (
+                                    match piece.black {
+                                        // White pawns can only move up
+                                        true => hovered_piece_pos.1 > piece.position.1 && hovered_piece_pos.1 - piece.position.1 == 1,
+                                        // Black pawns can only move down
+                                        false => hovered_piece_pos.1 < piece.position.1 && piece.position.1 - hovered_piece_pos.1 == 1,
+                                    }
+                                ) &&
+
+                                (
+                                    // Check that the pawn is moving straight forward
+                                    piece.position.0 == hovered_piece_pos.0
+
+                                    // If the player isn't moving straight forward, then do some checks
+                                    || 
+                                    (
+                                        {
+                                            moved_diagnally = 
+                                            // First, check that there is an enemy piece where the player is trying to move
+                                            piece_under_mouse.is_some() && 
+                                            // Make sure that if the player is trying to move diagnally, they're only trying to move by 1 left or right
+                                            (piece_under_mouse.unwrap().position.0 + 1 == piece.position.0 || piece_under_mouse.unwrap().position.0 - 1 == piece.position.0);
+
+
+                                            moved_diagnally
+                                        }
+                                    )
+                                )
+
+                            },
+                            _ => false,
+
+                        };
+
+
+                        match legal_move {
+                            true => match piece_under_mouse {
+                                // One of the pieces can be black, but not both for it to be able to move
+                                Some(piece_under_mouse) => (piece.black ^ piece_under_mouse.black && moved_diagnally, moved_diagnally),
+                                None => (true, false)
+                            }
+                            false => (false, false)
+
+                        }
+
+
+                    };
+
+                    if can_move {
+                        if should_kill {
+                            if let Some(piece_under_mouse) = pieces.iter_mut().find(|piece| piece.position == hovered_piece_pos) {
+                                piece_under_mouse.piece_type = PieceType::Dead;
+
+                            }
+                            
+
+                        }
+
+
+                        let piece = pieces.iter_mut().find(|piece| piece.position == selected_piece.unwrap() && piece.piece_type != PieceType::Dead).unwrap();
+
+                        piece.position = hovered_piece_pos;
+                        selected_piece = None;
 
                     }
+
+                // There is no piece selected, so it selects the piece the player is currently hovering over
+                } else if let Some(piece) = pieces.iter_mut().find(|piece| piece.position == hovered_piece_pos && piece.piece_type != PieceType::Dead) {
+                    selected_piece = match selected_piece.is_none() {
+                        true => Some(piece.position),
+                        false => None,
+                    };
 
                 }
             }
@@ -147,15 +222,6 @@ async fn main() {
 
         next_frame().await
     }
-
-
-    // Move the piece
-    /*if mouse_down && *piece != Piece::Empty {
-        if let Some(sel_piece) = selected_piece {
-            board[sel_piece.1][sel_piece.0] = *piece;
-            selected_piece = None;
-        }
-    }*/
 
 }
 
@@ -209,7 +275,10 @@ fn draw_board(piece_size: f32, pieces: &[Piece], selected_piece: Option<(u8, u8)
         let adj_y = piece.position.1 as f32 * piece_size;            
         
         // The text color should be the opposite of the board color
-        draw_text(piece_text, adj_x + piece_size / 2.0 - 25.0, adj_y + piece_size / 2.0, 50.0, piece.color);
+        draw_text(piece_text, adj_x + piece_size / 2.0 - 25.0, adj_y + piece_size / 2.0, 50.0, match piece.black {
+            true => BLACK,
+            false => WHITE,
+        });
 
     });
 
@@ -228,53 +297,3 @@ fn draw_board(piece_size: f32, pieces: &[Piece], selected_piece: Option<(u8, u8)
     }
 
 }
-
-            // Depending on the position of the piece, the piece un
-            /*let black = match y.is_even() {
-                true => x.is_odd(),
-                false => x.is_even(),
-            };
-            
-            // Adjust the coordinates to the desired piece size
-            let adj_x = x as f32 * piece_size;
-            let adj_y = y as f32 * piece_size;
-
-            #[cfg(debug_assertions)]
-            if let Some(p) = selected_piece {
-                draw_text(&p.0.to_string(), 800.0, 200.0, 25.0, WHITE);
-                draw_text(&p.1.to_string(), 825.0, 200.0, 25.0, WHITE);
-            }
-
-            // Move the piece
-            if mouse_down && *piece != Piece::Empty && selected_piece.is_none() {
-                selected_piece = Some((x, y));
-            }
-
-            // If the mouse is in the rectangle, the color is gray. If the piece is selected, the color is gray If not, then the color alternates
-            let color = match selected_piece == Some((x, y)) {
-                true => DARKGRAY,
-                false => match mouse_in_rectangle((adj_x, adj_y), (piece_size, piece_size)) {
-                    false => match black {
-                         true => BLACK,
-                         false => WHITE,
-                     },
-                    true => GRAY,
-                 }
-            };
-
-            draw_rectangle(adj_x, adj_y, piece_size, piece_size, color);
-
-            let piece_text = match piece.piece_type {
-                Piece::Pawn => "P",
-                Piece::Rook => "R",
-                Piece::Knight => "Kn",
-                Piece::Bishop => "B",
-                Piece::King => "Ki",
-                Piece::Queen => "Q",
-            };
-            
-            // The text color should be the opposite of the board color
-            draw_text(piece_text, adj_x + piece_size / 2.0 - 25.0, adj_y + piece_size / 2.0, 50.0, match black {
-                true => WHITE,
-                false => BLACK,
-            });*/
